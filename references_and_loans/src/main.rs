e74af2858b1f0daa4782575d08847303e25aa88f
Andss-ye <andrex120tv@gmail.com>
2025-09-29 18:49:41 -0500

    feat: :sparkles: ejemplos de references

--- references_and_loans/src/main.rs
+++ references_and_loans/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hola");
+    let _len = calcular_longitud(&s1);
+    println!("La longitud de '{}' es {}. {}", s1, calcular_longitud(&s1), _len);
+    println!("el valor original es: {}", s1);
+
+    let mut s2 = String::from("adios");
+    modificar(&mut s2);
+    println!("el valor modificado es: {}", s2);
+
+    let mut s2 = String::from("andrew");
+    {
+        let _r1 = &mut s2;
+    }
+    let _r2 = &mut s2;
+
+    let _referencia_a_la_nada = colgar();
+}
+
+fn calcular_longitud(s: &String) -> usize {
+    s.len()
+}
+
+fn modificar(s: &mut String) {
+    s.push_str(" kart");
+}
+
+fn colgar() -> String {
+    let s = String::from("hello");
+    s
 }
